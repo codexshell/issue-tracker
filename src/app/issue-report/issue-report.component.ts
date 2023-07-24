e7cab8595a2f508e95dd7b42929ff56d3d60bd5d
codexshell <52836290+codexshell@users.noreply.github.com>
2023-07-24 16:52:31 +0300

    chore: add form validation for the issue-report component

--- src/app/issue-report/issue-report.component.ts
+++ src/app/issue-report/issue-report.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 import { IssueForm } from '../issue-form';
 import { Issue } from '../issue';
@@ -12,16 +12,30 @@ import { IssuesService } from '../issues.service';
 })
 export class IssueReportComponent {
   issueForm = new FormGroup<IssueForm>({
-    title: new FormControl('', { nonNullable: true }),
+    title: new FormControl('', {
+      nonNullable: true,
+      validators: Validators.required,
+    }),
     description: new FormControl('', { nonNullable: true }),
-    priority: new FormControl('', { nonNullable: true }),
-    type: new FormControl('', { nonNullable: true }),
+    priority: new FormControl('', {
+      nonNullable: true,
+      validators: Validators.required,
+    }),
+    type: new FormControl('', {
+      nonNullable: true,
+      validators: Validators.required,
+    }),
   });
   @Output() formClose = new EventEmitter();
 
   constructor(private issuesService: IssuesService) {}
 
   addIssue() {
+    if (this.issueForm && this.issueForm.invalid) {
+      this.issueForm.markAllAsTouched();
+      return;
+    }
+
     this.issuesService.createIssue(this.issueForm.getRawValue() as Issue);
     this.formClose.emit();
   }
